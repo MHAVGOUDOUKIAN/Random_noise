27d737cd6e23f51dbfe7ebb4fa7d38b75ff03df4
Hubks26 <alex.goussian@gmail.com>
2021-01-29 22:15:05 +0100

    Seed OK

--- src/Grid.cpp
+++ src/Grid.cpp
@@ -2,32 +2,10 @@
 
 Grid::Grid()
 {
-	m_table[0][0] = rand() % 245 + 10;
+	unsigned seed = std::chrono::steady_clock::now().time_since_epoch().count();
+	srand(seed);
 	
-	for(int i = 1; i < m_dim-1; ++i)
-	{
-		m_table[i][1] = rand() % 245 + 10;
-	}
-	for(int i = 1; i < m_dim-2; ++i)
-	{
-		m_table[1][i+1] = rand() % 245 + 10;
-	}
-	for(int i = 2; i < m_dim-1; ++i)
-	{
-		m_table[i][2] = rand() % 245 + 10;
-	}
-	for(int i = 1; i < m_dim-3; ++i)
-	{
-		m_table[2][i+2] = rand() % 245 + 10;
-	}
-	for(int i = 3; i < m_dim-1; ++i)
-	{
-		m_table[i][3] = rand() % 245 + 10;
-	}
-	for(int i = 1; i < m_dim-4; ++i)
-	{
-		m_table[3][i+3] = rand() % 245 + 10;
-	}
+	m_table[1][1] = rand() % 245 + 10;
 }
 
 void Grid::draw(sf::RenderTarget& target, sf::RenderStates states) const
@@ -59,7 +37,7 @@ void Grid::modif(int numRow, int numCol, int new_value)
 }
 
 void Grid::update()
-{	
+{
 	/*
 	bool newArray[m_dim][m_dim]{};
 	
--- src/Grid.h
+++ src/Grid.h
@@ -2,6 +2,7 @@
 #define GRID_H
 
 #include <SFML/Graphics.hpp>
+#include <chrono>
 
 class Grid : public sf::Drawable
 {
@@ -17,7 +18,7 @@ private :
 	
 private :
 	
-	static constexpr int m_dim = 50;
+	static constexpr int m_dim = 12;
 	int m_table[m_dim][m_dim]{};
 };
 
